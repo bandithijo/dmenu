6e831a885fce780edad517e9e66a29023b278785
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-05-08 21:06:19 +0800

    Change dmenu height to 3

--- dmenu.c
+++ dmenu.c
@@ -608,7 +608,7 @@ setup(void)
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
 
 	/* calculate menu geometry */
-	bh = drw->fonts->h + 2;
+	bh = drw->fonts->h + 3;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
 #ifdef XINERAMA
