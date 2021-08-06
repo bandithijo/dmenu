a78e55d3ede0f1382f5265381b72ed85ebb36589
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-08-07 06:07:36 +0800

    Add joypixels emoji

--- config.def.h
+++ config.def.h
@@ -9,10 +9,11 @@ static int min_width   = 500;    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font Bandit:style=Medium,Regular:size=8",
-	"Font Awesome 5 Free:style=Regular:size=8",
-	"Font Awesome 5 Brands:style=Regular:size=8",
-	"octicons:style=Medium:size=8"
+    "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+	"octicons:style=Medium:size=8",
 };
+	/* "Font Awesome 5 Free:style=Regular:size=8", */
+	/* "Font Awesome 5 Brands:style=Regular:size=8", */
 static const char *prompt   = NULL;      /* -p  option; prompt to the left of input field */
 static const char *symbol_1 = "";
 static const char *symbol_2 = "";
