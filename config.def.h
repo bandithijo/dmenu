d24bed53e7b71e9c2911247852e9b28e8564f7fd
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-12-13 22:52:19 +0800

    Add barheight config

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar      = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int user_bh     = 18;     /* bar height */
 static int fuzzy       = 1;      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int colorprompt = 1;      /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 static int centered    = 0;      /* -c option; centers dmenu on screen */
