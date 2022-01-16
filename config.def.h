b1caeb23a7e60f4a34ab757aadee214dcd0c17fa
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-01-16 10:58:40 +0800

    Change user_bh into 20 and Add Twemoji font

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar      = 1;      /* -b  option; if 0, dmenu appears at bottom     */
-static int user_bh     = 18;     /* bar height */
+static int user_bh     = 20;     /* bar height, default 18 */
 static int fuzzy       = 1;      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int colorprompt = 1;      /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 static int centered    = 0;      /* -c option; centers dmenu on screen */
@@ -10,9 +10,10 @@ static int min_width   = 500;    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font Bandit:style=Medium:pixelsize=12",
-    "JoyPixels:pixelsize=12:antialias=true:autohint=true",
-	"octicons:style=Medium:pixelsize=12",
+    "Twemoji:pixelsize=12:antialias=true:autohint=true"
 };
+    /* "JoyPixels:pixelsize=12:antialias=true:autohint=true", */
+	/* "octicons:style=Medium:pixelsize=12", */
 	/* "Font Awesome 5 Free:style=Regular:size=8", */
 	/* "Font Awesome 5 Brands:style=Regular:size=8", */
 static const char *prompt   = NULL;      /* -p  option; prompt to the left of input field */
