96f4491165ba9db3b2c123709a9e0669957718d6
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-07-24 07:50:21 +0800

    Change font 15 to 17; Change user_bh 24 to 26

--- config.def.h
+++ config.def.h
@@ -2,20 +2,20 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar      = 1;      /* -b  option; if 0, dmenu appears at bottom     */
-static int user_bh     = 24;     /* bar height, default 18 */
+static int user_bh     = 26;     /* bar height, default 18 */
 static int fuzzy       = 1;      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int colorprompt = 1;      /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 static int centered    = 0;      /* -c option; centers dmenu on screen */
 static int min_width   = 500;    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font Bandit:style=Medium:pixelsize=15",
-        "Twemoji:pixelsize=15:antialias=true:autohint=true"
+	"JetBrainsMono Nerd Font Bandit:style=Medium:pixelsize=17",
+        "Twemoji:pixelsize=17:antialias=true:autohint=true"
 };
-        /* "JoyPixels:pixelsize=15:antialias=true:autohint=true", */
-	/* "octicons:style=Medium:pixelsize=15", */
-	/* "Font Awesome 5 Free:style=Regular:size=8", */
-	/* "Font Awesome 5 Brands:style=Regular:size=8", */
+        /* "JoyPixels:pixelsize=17:antialias=true:autohint=true", */
+	/* "octicons:style=Medium:pixelsize=17", */
+	/* "Font Awesome 5 Free:style=Regular:size=17", */
+	/* "Font Awesome 5 Brands:style=Regular:size=17", */
 static const char *prompt   = NULL;      /* -p  option; prompt to the left of input field */
 static const char *symbol_1 = "";
 static const char *symbol_2 = "";
